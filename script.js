a2bb4368514cc484e7ab2c44409dff91f832268e
mastergrillfwcafe <mastergrillfw.cafe@gmail.com>
2025-10-03 22:55:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
   alert("Hello! Thanks for visiting my website 😊");
 }
+
+function submitForm() {
+  alert("Thank you for your message! We’ll get back to you soon.");
+  return false; // prevents actual form submission (for now)
+}
